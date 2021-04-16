7001e700553fd9282891bd3936ee6b940264bb8b
cclews <codyclews@gmail.com>
2021-04-16 19:55:22 -0400

    all interactive routing complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import {
   AppBar,
   TextField,
+  Box,
   Button,
   Toolbar,
   Typography,
@@ -9,15 +10,16 @@ import {
   CardActions,
   CardContent,
   IconButton,
+  Fab,
 } from '@material-ui/core';
-import { Delete } from '@material-ui/icons';
+import qs from 'qs';
+import { Delete, ThumbUp, DoubleArrow } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
 import axios from 'axios';
 
 const useStyles = makeStyles({
   container: {
     minHeight: '100vh',
-    width: '100vw',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -26,7 +28,7 @@ const useStyles = makeStyles({
   },
   root: {
     marginBottom: '0.5rem',
-    minWidth: '20rem',
+    width: '20rem',
   },
   buttonWrapper: {
     display: 'flex',
@@ -34,11 +36,81 @@ const useStyles = makeStyles({
     width: '100%',
     justifyContent: 'space-between',
   },
+  formContainer: {
+    width: '20rem',
+    maxWidth: '100%',
+    marginBottom: '1rem',
+    padding: '1rem',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'end',
+  },
+  form: {
+    width: '100%',
+  },
 });
 
-function Post({ postData }) {
-  const [posts, setPosts] = useState([]);
+function Post({ postData, getPosts }) {
   const classes = useStyles();
+
+  const [liked, setLiked] = useState(false);
+
+  let likeCount = postData.likes + liked;
+
+  async function handleLike() {
+    if (!liked) {
+      try {
+        const like = await axios({
+          method: 'post',
+          url: '/like',
+          data: qs.stringify({
+            id: postData.id,
+          }),
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+          },
+        });
+        setLiked(!liked);
+      } catch (err) {
+        alert(err.message);
+      }
+    } else {
+      try {
+        const like = await axios({
+          method: 'post',
+          url: '/unlike',
+          data: qs.stringify({
+            id: postData.id,
+          }),
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+          },
+        });
+        setLiked(!liked);
+      } catch (err) {
+        alert(err.message);
+      }
+    }
+  }
+
+  async function handleDelete() {
+    try {
+      const removed = await axios({
+        method: 'delete',
+        url: '/remove',
+        data: qs.stringify({
+          id: postData.id,
+        }),
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+        },
+      });
+      alert(removed.data.message);
+      getPosts();
+    } catch (err) {
+      alert(err.message);
+    }
+  }
 
   return (
     <Card className={classes.root}>
@@ -47,10 +119,18 @@ function Post({ postData }) {
       </CardContent>
       <CardActions>
         <div className={classes.buttonWrapper}>
-          <Button size='small' endIcon={<Delete />}>
-            {postData.likes ? `${postData.likes} Likes` : 'Like'}
+          <Button
+            color='primary'
+            variant={liked ? 'contained' : 'default'}
+            size='small'
+            endIcon={<ThumbUp />}
+            onClick={handleLike}
+          >
+            {likeCount
+              ? `${likeCount} Like${likeCount === 1 ? '' : 's'}`
+              : 'Like'}
           </Button>
-          <IconButton aria-label='delete post'>
+          <IconButton aria-label='delete post' onClick={handleDelete}>
             <Delete />
           </IconButton>
         </div>
@@ -60,35 +140,81 @@ function Post({ postData }) {
 }
 function App() {
   const classes = useStyles();
+
+  const [form, setForm] = useState();
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState('');
 
+  const getPosts = async () => {
+    const posts = await axios.get('/posts').then((res) => {
+      if (res.status === 500) {
+        setError(res.error);
+      } else {
+        console.log(res.data);
+        setPosts(res.data.posts);
+      }
+    });
+    console.log(posts);
+  };
+
   useEffect(() => {
-    const getPosts = async () => {
-      const posts = await axios.get('/posts').then((res) => {
-        if (res.status === 500) {
-          setError(res.error);
-        } else {
-          console.log(res.data);
-          setPosts(res.data.posts);
-        }
-      });
-      console.log(posts);
-    };
     getPosts();
   }, []);
 
+  async function submitPost() {
+    try {
+      if (form.length) {
+        const post = await axios({
+          method: 'post',
+          url: '/posts',
+          data: qs.stringify({
+            content: form,
+          }),
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+          },
+        });
+        setForm('');
+        getPosts();
+      } else {
+        throw new Error('You must put something in your post!');
+      }
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
   return (
     <div className='App'>
-      <AppBar position='static'>
+      <AppBar position='sticky'>
         <Toolbar>
           <Typography variant='h6'>FINBACK670 Takehome</Typography>
         </Toolbar>
       </AppBar>
       <div className={classes.container}>
+        <Card className={classes.formContainer}>
+          <TextField
+            className={classes.form}
+            label='Enter your post here!'
+            value={form}
+            multiline
+            onChange={(e) => setForm(e.target.value)}
+          />
+          <CardActions>
+            <Button
+              variant='contained'
+              className={classes.submit}
+              color='primary'
+              onClick={submitPost}
+              endIcon={<DoubleArrow />}
+            >
+              Submit
+            </Button>
+          </CardActions>
+        </Card>
         {posts
           ? posts.map((post) => {
-              return <Post postData={post} />;
+              return <Post postData={post} getPosts={getPosts} />;
             })
           : null}
       </div>
